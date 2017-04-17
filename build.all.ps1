ae813f81e68446ff22aafda82974a5ea2c2d1f24
Regan Laitila <github@reganlaitila.net>
2017-04-17 00:30:17 -0400

    updated build and move cli cmd

--- build.all.ps1
+++ build.all.ps1
@@ -1,8 +1,10 @@
-$env:GOOS='windows';$env:GOARCH='amd64';go install std; go build -o $env:GOPATH\bin\deviceio-cli.windows.amd64.exe github.com/deviceio/cli 
-$env:GOOS='windows';$env:GOARCH='386';go install std; go build -o $env:GOPATH\bin\deviceio-cli.windows.386.exe github.com/deviceio/cli 
+$gocmd='github.com/deviceio/cli/cmd/deviceio-cli'
 
-$env:GOOS='linux';$env:GOARCH='amd64';go install std; go build -o $env:GOPATH\bin\deviceio-cli.linux.amd64 github.com/deviceio/cli
-$env:GOOS='linux';$env:GOARCH='386';go install std; go build -o $env:GOPATH\bin\deviceio-cli.linux.386 github.com/deviceio/cli
+$env:GOOS='windows';$env:GOARCH='amd64';go install std; go build -o $env:GOPATH\bin\deviceio-cli.windows.amd64.exe $gocmd 
+$env:GOOS='windows';$env:GOARCH='386';go install std; go build -o $env:GOPATH\bin\deviceio-cli.windows.386.exe $gocmd 
 
-$env:GOOS='darwin';$env:GOARCH='amd64';go install std; go build -o $env:GOPATH\bin\deviceio-cli.darwin.amd64 github.com/deviceio/cli
-$env:GOOS='darwin';$env:GOARCH='386';go install std; go build -o $env:GOPATH\bin\deviceio-cli.darwin.386 github.com/deviceio/cli
+$env:GOOS='linux';$env:GOARCH='amd64';go install std; go build -o $env:GOPATH\bin\deviceio-cli.linux.amd64 $gocmd
+$env:GOOS='linux';$env:GOARCH='386';go install std; go build -o $env:GOPATH\bin\deviceio-cli.linux.386 $gocmd
+
+$env:GOOS='darwin';$env:GOARCH='amd64';go install std; go build -o $env:GOPATH\bin\deviceio-cli.darwin.amd64 $gocmd
+$env:GOOS='darwin';$env:GOARCH='386';go install std; go build -o $env:GOPATH\bin\deviceio-cli.darwin.386 $gocmd
